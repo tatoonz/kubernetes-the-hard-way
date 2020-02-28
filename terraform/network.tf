940ab38dae1911401e20bb8d6565ee4827bbf5af
Peerapat Naksumpan <peerapath.t@gmail.com>
2020-02-28 15:57:43 +0700

    allow firewall for both internal and external

--- terraform/network.tf
+++ terraform/network.tf
@@ -8,3 +8,38 @@ resource "google_compute_subnetwork" "kubernetes" {
   ip_cidr_range = "10.240.0.0/24"
   network = google_compute_network.vpc.self_link
 }
+
+resource "google_compute_firewall" "internal" {
+  name    = "kubernetes-the-hard-way-allow-internal"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["10.240.0.0/22", "10.200.0.0/16"]
+}
+
+resource "google_compute_firewall" "external" {
+  name    = "kubernetes-the-hard-way-allow-external"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports = ["22", "6443"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
