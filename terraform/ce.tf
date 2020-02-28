0bc337365984677b6787e06d430fcc831a3ea8a4
Peerapat Naksumpan <peerapath.t@gmail.com>
2020-02-28 17:20:19 +0700

    provision k8s workers

--- terraform/ce.tf
+++ terraform/ce.tf
@@ -23,3 +23,33 @@ resource "google_compute_instance" "k8s_controller" {
     scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
   }
 }
+
+resource "google_compute_instance" "k8s_worker" {
+  count = 3
+  name = "worker-${count.index}"
+  machine_type = "n1-standard-1"
+  tags = ["kubernetes-the-hard-way", "worker"]
+  zone = "asia-southeast1-b"
+
+  metadata = {
+    pod-cidr = "10.200.${count.index}.0/24"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+      size = 200
+    }
+  }
+
+  can_ip_forward = true
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.kubernetes.self_link
+    network_ip = "10.240.0.2${count.index}"
+  }
+
+  service_account {
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
+  }
+}
